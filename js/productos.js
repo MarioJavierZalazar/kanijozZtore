290566191fd2f5cb9d0a6962b04df7aad75e599a
Usuario <mariojavierzalazar@gmail.com>
2023-07-27 23:02:09 -0300

    inicializamos git

---
+++ js/productos.js
@@ -0,0 +1,56 @@
+let products = [];
+
+class Product {
+    constructor(id, name, price, text, picture) {
+        this.id = id,
+        this.name = name,
+        this.price = price;
+        this.text = text;
+        this.picture = picture;
+    }
+};
+const grillaDePorductosUI = (lista) =>{
+    for (const product of lista) {
+        $('#listaDeProductos').append(`
+        <div class="col-12 col-md-3 mb-5 rounded">
+                <div class="cardImg">
+                    <img src=${product.picture} class="card-img-top img-fluid" alt=${product.name} data-bs-toggle="modal" data-bs-target="#modal${product.name}">
+                </div>
+                <div class="cardText p-3 bg-dark textoPrimario rounded-bottom">
+                    <h5 class="text-center fw-bold">${product.name}</h5>
+                    <p class="card-text">${product.text}</p>
+                    <div class="d-flex justify-content-around align-items-baseline">
+                        <a target="_blank" href="https://api.whatsapp.com/send?phone=541173692881&text=Hola%20me%20interesa%20este%20producto%20${product.name}"
+                        class="btn">Contactar</a>
+                        <div>
+                            <p>$${product.price}</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <!-- Modal -->
+                <div class="modal fade" id="modal${product.name}" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-lg">
+                        <div class="modal-content">
+                            <div>
+                                <img src=${product.picture} class="card-img-top img-fluid" alt=${product.name}>
+                                <button type="button" class="btn-close position-absolute top-0 end-0 m-3" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    }
+}
+
+$.get('../data/products.json', (respuesta, estado) => {
+    if (estado == 'success') {
+        for (const product of respuesta){
+            products.push(new Product(product.id,product.name,product.price,product.text,product.picture))
+        }
+        grillaDePorductosUI(products.reverse());
+    } else {
+        console.log('Error en la carga de datos');
+    };
+})
