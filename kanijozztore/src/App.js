b03bd1f7415c1cb0da4320637e087eff4ead6a3f
Usuario <mariojavierzalazar@gmail.com>
2023-07-30 19:35:28 -0300

    despues de 20 intentes se crea react con vainillas css

---
+++ kanijozztore/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Tienda from './components/Tienda';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Tienda/>
+    </>
+  );
+}
+
+export default App;
